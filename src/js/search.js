ff10074b5a4cbff21f84a97ebbc095d24f800644
Ryan O'Day <romanguy722@gmail.com>
2015-11-14 20:04:05 -0500

    Fixed Errors

--- src/js/search.js
+++ src/js/search.js
@@ -1,5 +1,5 @@
 /*
-Developed by Ryan O'Day and Jack Zheng
+Developed by Ryan O'Day
 HackRPI 2015 Project
 */
 //-----------------API Keys--------------------------------
@@ -7,52 +7,65 @@ HackRPI 2015 Project
 //---------------------------------------------------------
 
 function getLocation(){
+	console.log("Hello World!");
 	var loc=[];
  	var address = document.getElementById("searchbox").value;
   var apikey_tripadvisor = "6df2666f-a98d-4502-84a4-8b70540e0628";
   var locid;
   var loctype;
   
-  //Get latitude and longitude
-  geocoder.geocode( { 'address': address}, function(results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
-			loc[0]=results[0].geometry.location.lat();
-      loc[1]=results[0].geometry.location.lng();
+  var results_parser = function(results, status) {
+	if (status == google.maps.GeocoderStatus.OK) {
+		loc[0]=results[0].geometry.location.lat();
+		loc[1]=results[0].geometry.location.lng();
+		console.log("Lat & Longitude (in results_parser):",loc);
+		var idurl = "http://api.tripadvisor.com/api/partner/2.0/map/" + loc[0] + "," + loc[1] + "?key=" + apikey_tripadvisor;
+		console.log("URI:",idurl);
+		 $.ajax({
+					type: "GET",
+					cache: true,
+					url: idurl,
+					dataType: 'json',
+					success: function(result){
+					  var data = result.data;
+					  console.log("Got result data:", data);
+					  locid = data[0].location_id;
+					  console.log("Loc id:",locid);
+					  //Get tripadvisor category
+						var caturl = "http://api.tripadvisor.com/api/partner/2.0/location/" + locid + "?key=" + apikey_tripadvisor;
+						console.log("URI (round 2):",caturl);
+						 $.ajax({
+									type: "GET",
+									cache: true,
+									url: caturl,
+									dataType: 'json',
+									success: function(response){
+										console.log("Response success:",response.attraction_types[0].name);
+									},
+									error: function (msg) {
+										console.log("Response failure:", msg);
+												var errOutput = document.getElementById('error');
+													errOutput.innerHTML = msg.responseText;
+									}
+								});
+					},
+					error: function (msg) {
+								var errOutput = document.getElementById('error');
+									errOutput.innerHTML = msg.responseText;
+					}
+				});
     }else{
     	var errOutput = document.getElementById('error');
     	errOutput.innerHTML = "There was an error attempting to locate the entered location.";
   	}
+  }
+  
+  var geocoder = new google.maps.Geocoder();
+  //Get latitude and longitude
+  geocoder.geocode( { 'address': address}, results_parser );
   //------------------------
   //Get tripadvisor location id
-    var idurl = "http://api.tripadvisor.com/api/partner/2.0/map/" + loc[0] + "," + loc[1] + "?key=" + apikey_tripadvisor;
-     $.ajax({
-                type: "GET",
-                cache: true,
-                url: idurl,
-                dataType: 'json',
-                success: function(data){
-                  locid = data[0].location_id;
-                },
-                error: function (msg) {
-      						var errOutput = document.getElementById('error');
-    							errOutput.innerHTML = msg.responseText;
-                }
-            });
+    
   //-------------------------
-  //Get tripadvisor category
-    var caturl = "http://api.tripadvisor.com/api/partner/2.0/location/" + locid + "?key=" + apikey_tripadvisor;
-     $.ajax({
-                type: "GET",
-                cache: true,
-                url: caturl,
-                dataType: 'json',
-                success: function(data){
-                  loctype = data.subcategory.;
-                  alert(data);
-                },
-                error: function (msg) {
-      						var errOutput = document.getElementById('error');
-    							errOutput.innerHTML = msg.responseText;
-                }
-            });
+  
   }
